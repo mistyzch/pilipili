776b373474308c879255b574cda6769bfb00729f
mistyzch <1634064086@qq.com>
2019-12-24 15:42:32 +0800

    dao层

--- src/main/java/com/pilipili/dao/UserDao.java
+++ src/main/java/com/pilipili/dao/UserDao.java
@@ -1,11 +1,13 @@
 package com.pilipili.dao;
 
+import com.pilipili.entity.User;
+
 /**
  * Create by misty on 2019/12/24 11:37
  */
 public interface UserDao {
 
-    public User selectUserByAccountAndPwd(String account,String password);
+    public User selectUserByAccountAndPwd(String account, String password);
 
 
 
