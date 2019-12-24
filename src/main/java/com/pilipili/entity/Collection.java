1777416079a51e452aee162797b0e38499e30f9a
BrucessKING <39517489+BrucessKING@users.noreply.github.com>
2019-12-24 16:39:13 +0800

    增加目录，收藏，视频实体类

--- src/main/java/com/pilipili/entity/Collection.java
+++ src/main/java/com/pilipili/entity/Collection.java
@@ -1,9 +1,67 @@
 package com.pilipili.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * @author: ABKing
  * @Date: 2019/12/24 15:42
  * @Description: 收藏
  **/
-public class Collection {
+public class Collection implements Serializable {
+    private Integer id;
+    private Date date;
+    private String state;
+    private User user;
+    private Video video;
+
+    public Collection() {
+    }
+
+    public Collection(Date date, String state, User user, Video video) {
+        this.date = date;
+        this.state = state;
+        this.user = user;
+        this.video = video;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
 }
